e462e2f36c06b2a469d2967109fd4dbc71ff8437
Richard Ekwonye <ekwonyenoob@gmail.com>
2018-02-24 10:03:09 +0100

    fixed scene light

--- src/components/MoonScene/MoonScene.js
+++ src/components/MoonScene/MoonScene.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
-import * as ObjectLoader from 'three-obj-loader';
-import OBJ from '../../shared/Moon_Model/moon.obj';
 import TXT_IMG from '../../shared/Moon_Model/MoonMap2_2500x1250.jpg';
-import BUMP_MAP from '../../shared/Moon_Model/moon-normal.jpg';
-
-import texture_image from '../../shared/images/planet-512.jpg';
-import nMap from '../../shared/images/normal-map-512.jpg';
-import sMap from '../../shared/images/water-map-512.jpg';
 
 const CANVAS_WIDTH = 1440;
 const CANVAS_HEIGHT = 1050;
@@ -40,106 +33,34 @@ class MoonScene extends Component {
     this.camera.position.z = 215;
     this.camera.lookAt(this.scene.position);
 
-    const ambientLight = new THREE.AmbientLight(0x7d7874, 0.5);
-    // this.scene.add(ambientLight);
-
-    let spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(40, 40, 70);
+    let spotLight = new THREE.SpotLight(0xaaa4a1);
+    spotLight.position.set(60, 10, 125);
     spotLight.intensity = 2;
     this.scene.add(spotLight);
 
-    // const spotLight = new THREE.SpotLight(0xffffff, 0.4);
-    // spotLight.position.set(590, 180, 500);
-    // spotLight.castShadow = true;
+    var moonGeometry = new THREE.SphereGeometry(65, 350, 350);
 
-    // spotLight.power = 8;
-    // spotLight.distance = 80000;
+    var texture = THREE.ImageUtils.loadTexture(TXT_IMG);
 
-    // this.scene.add(spotLight);
+    var moonMaterial = new THREE.MeshPhongMaterial();
+    moonMaterial.map = texture;
+    moonMaterial.shininess = 1;
+    moonMaterial.normalScale.set(-0.3, -0.3);
 
-    const scene = this.scene;
-    const threeLoader = THREE;
-    const OBJLoader = ObjectLoader;
-    OBJLoader(threeLoader);
+    var moon = new THREE.Mesh(moonGeometry, moonMaterial);
 
-    const manager = new THREE.LoadingManager();
-    const loader = new THREE.ImageLoader(manager);
+    moon.material.map.wrapS = THREE.RepeatWrapping;
+    moon.material.map.wrapT = THREE.RepeatWrapping;
+    moon.material.map.repeat.set(2, 1);
 
-    const moonTexture = new THREE.Texture();
+    moon.position.x = 15;
+    moon.position.y = window.innerHeight < 840 ? 10 : 0;
+    moon.position.z = 0;
+    moon.rotation.x = 0.22;
+    moon.rotation.y = -9;
+    moon.name = 'moon';
 
-    loader.load(TXT_IMG, image => {
-      moonTexture.image = image;
-      moonTexture.needsUpdate = true;
-    });
-
-    const meshes = [];
-
-    const objLoader = new threeLoader.OBJLoader();
-
-    objLoader.load(OBJ, object => {
-      object.traverse(child => {
-        if (child instanceof THREE.Mesh) {
-          meshes.push(child);
-        }
-      });
-
-      const moonObj = meshes[0];
-      moonObj.name = 'moonModel';
-      moonObj.position.x = 20;
-      moonObj.position.y = 15;
-      moonObj.rotation.x = 0.22;
-      moonObj.rotation.y = -2;
-
-      scene.add(moonObj);
-
-      const moonBumpMap = new THREE.TextureLoader().load(BUMP_MAP);
-
-      moonObj.material = new THREE.MeshPhongMaterial({
-        map: moonTexture,
-        bumpMap: moonBumpMap,
-        specular: 0xfff7e8,
-        bumpScale: 1
-      });
-    });
-
-    var planetGeometry = new THREE.SphereGeometry(60, 300, 300);
-
-    //Load the planet textures
-    var texture = THREE.ImageUtils.loadTexture(texture_image);
-    var normalmap = THREE.ImageUtils.loadTexture(nMap);
-    var specmap = THREE.ImageUtils.loadTexture(sMap);
-
-    var planetMaterial = new THREE.MeshPhongMaterial();
-    planetMaterial.map = texture;
-
-    planetMaterial.specularMap = specmap;
-    planetMaterial.specular = new THREE.Color(0xff0000);
-    planetMaterial.shininess = 1;
-
-    planetMaterial.normalMap = normalmap;
-    planetMaterial.normalScale.set(-0.3, -0.3);
-
-    var planet = new THREE.Mesh(planetGeometry, planetMaterial);
-
-    //here we allow the texture/normal/specular maps to wrap
-    planet.material.map.wrapS = THREE.RepeatWrapping;
-    planet.material.map.wrapT = THREE.RepeatWrapping;
-    planet.material.normalMap.wrapS = THREE.RepeatWrapping;
-    planet.material.normalMap.wrapT = THREE.RepeatWrapping;
-    planet.material.specularMap.wrapS = THREE.RepeatWrapping;
-    planet.material.specularMap.wrapT = THREE.RepeatWrapping;
-
-    //here we repeat the texture/normal/specular maps twice along X
-    planet.material.map.repeat.set(2, 1);
-    planet.material.normalMap.repeat.set(2, 1);
-    planet.material.specularMap.repeat.set(2, 1);
-
-    planet.position.x = 0;
-    planet.position.y = 0;
-    planet.position.z = 0;
-    planet.name = 'planet';
-
-    // scene.add(planet);
+    this.scene.add(moon);
 
     this.renderer.setPixelRatio(window.devicePixelRatio);
 
@@ -155,8 +76,7 @@ class MoonScene extends Component {
       return;
     }
 
-    // const model = this.scene.getObjectByName('moonModel');
-    const model = this.scene.getObjectByName('planet');
+    const model = this.scene.getObjectByName('moon');
     requestAnimationFrame(this.modelAnimateIn);
 
     if (model) {
@@ -179,8 +99,7 @@ class MoonScene extends Component {
     }
 
     if (previousMousePosition) {
-      // const model = this.scene.getObjectByName('moonModel');
-      const model = this.scene.getObjectByName('planet');
+      const model = this.scene.getObjectByName('moon');
 
       const deltaMove = {
         x: e.screenX - previousMousePosition.x,
